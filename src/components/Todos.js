e0fe5a52605d72a179147208ada419422352f5e3
shahid yousafxai <shahidbahadur1998@gmail.com>
2021-11-05 06:20:21 -0700

    simplify

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Todos.css'
 
-function Todos({ todos, deleteTodo, toggleClick, pending }) {
+function Todos({ todos, deleteTodo }) {
   // const stateVal = pending.filter((statetodo) => {
   //   return console.log(statetodo.id)
   // })
